83351f5d322112f6f854bb722e8891e4b1890630
Zhaslan Yesnazarov <zyesnazarov@gmail.com>
2024-11-28 18:12:05 +0100

    some updates

--- app.py
+++ app.py
@@ -27,6 +27,20 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regression_model.predict(final_input)[0]
+    return render_template('home.html',predict_text = 'Predicted house price is {}'.format(output))
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
